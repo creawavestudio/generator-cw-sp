4a1ef535aca42f51e935fda9eef5c60dea357a17
Defus-E <denis.ryb186f@gmail.com>
2021-03-08 20:23:55 +0200

    Updated syntax

--- generators/app/dependencies/babel.js
+++ generators/app/dependencies/babel.js
@@ -1,17 +1,17 @@
 module.exports = {
-	"babel": {
-		"presets": [
-			"@babel/preset-env",
-			"@babel/preset-react"
-		]
-	},
-	"devDependencies": {
-		"@babel/core": "^7.12.13",
-		"@babel/preset-env": "^7.12.13",
-		"@babel/preset-react": "^7.12.13",
-		"babel-cli": "^6.26.0",
+  "babel": {
+    "presets": [
+      "@babel/preset-env",
+      "@babel/preset-react"
+    ]
+  },
+  "devDependencies": {
+    "@babel/core": "^7.12.13",
+    "@babel/preset-env": "^7.12.13",
+    "@babel/preset-react": "^7.12.13",
+    "babel-cli": "^6.26.0",
     "babel-core": "^6.26.3",
     "babel-preset-es2015": "^6.24.1",
-		"gulp-babel": "^8.0.0"
-	}
+    "gulp-babel": "^8.0.0"
+  }
 }
